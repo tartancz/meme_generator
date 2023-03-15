84234381ec76be0c28864c5f69bc19e84079e3ce
ruzicka <tartanec@gmail.com>
2023-03-15 14:32:52 +0100

    added static to url

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -25,3 +27,5 @@ router.register(r"pokus", ImageView)
 urlpatterns = [path("admin/", admin.site.urls),
                path("", include(router.urls)),
                ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

ffa1e094a7c3bf3d683723f7a67fc53c83bafddf
ruzicka <tartanec@gmail.com>
2023-03-20 15:25:36 +0100

    temp save

--- backend/src/memes/views.py
+++ backend/src/memes/views.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
@@ -5,6 +7,7 @@ from rest_framework.response import Response
 
 from .models import MemeTemplate
 from .serializers import MemeTemplateSerializer, GenerateMemeSerializer
+from .utils import Memer
 
 
 class ImageView(mixins.ListModelMixin,
@@ -27,9 +30,21 @@ class ImageView(mixins.ListModelMixin,
 
     @action(methods=["post"], detail=True)
     def generate(self, request, pk):
-        ser = self.serializer_class
-        print(ser)
+        '''
+        Will generate new meme
+        '''
 
+        ser: GenerateMemeSerializer = self.get_serializer(data=request.data)
+        ser.is_valid(raise_exception=True)
+        bottom_text = ser.validated_data["bottom_text"]
+        top_text = ser.validated_data["top_text"]
+        #getting file
         meme_template = get_object_or_404(MemeTemplate, pk=pk)
         file = meme_template.high_res.file
+        #creating meme
+        with Memer(file) as memer:
+            memer.generate_meme(bottom_text, top_text)
+            image = memer.get_image()
+        with open("pokus.png", 'ab') as bagr:
+            bagr.write(image)
         return Response({"asd": "asd"})

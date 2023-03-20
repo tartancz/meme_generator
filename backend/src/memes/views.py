6f82d0c3288237a99de6c7782369a49f85f97e5e
ruzicka <tartanec@gmail.com>
2023-03-20 07:55:41 +0100

    new action generate (not completed) and updated get_serializer_class to show right serializer

--- backend/src/memes/views.py
+++ backend/src/memes/views.py
@@ -1,14 +1,35 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import MemeTemplate
-from .serializers import MemeTemplateSerializer
+from .serializers import MemeTemplateSerializer, GenerateMemeSerializer
 
 
 class ImageView(mixins.ListModelMixin,
                 mixins.RetrieveModelMixin,
                 viewsets.GenericViewSet):
-    queryset = MemeTemplate.objects.all()
-    serializer_class = MemeTemplateSerializer
-
     class Meta:
         model = MemeTemplate
+
+    queryset = MemeTemplate.objects.all()
+
+    serializers = {
+        'generate': GenerateMemeSerializer,
+        'list': MemeTemplateSerializer,
+        'retrieve': MemeTemplateSerializer,
+    }
+
+    def get_serializer_class(self):
+        serializer = self.serializers.get(self.action)
+        return serializer
+
+    @action(methods=["post"], detail=True)
+    def generate(self, request, pk):
+        ser = self.serializer_class
+        print(ser)
+
+        meme_template = get_object_or_404(MemeTemplate, pk=pk)
+        file = meme_template.high_res.file
+        return Response({"asd": "asd"})

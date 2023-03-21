ac205aeb99114dcb4a639e65843ca8f3e199e43b
ruzicka <tartanec@gmail.com>
2023-03-21 09:03:36 +0100

    to Memer added extra validation and methods open and close

--- backend/src/memes/utils.py
+++ backend/src/memes/utils.py
@@ -1,15 +1,20 @@
+import io
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from PIL import ImageDraw, Image, ImageFont
-import io
+
+if TYPE_CHECKING:
+    from PIL.ImageFile import ImageFile
+    from PIL.ImageDraw import ImageDraw
 
 
 class Memer:
     def __init__(self, file, font_size: int = 32):
         self.file = file
         self.font = self._create_font(size=font_size)
-        self.image = None
-        self.draw = None
+        self.image: ImageFile | None = None
+        self.draw: ImageDraw | None = None
 
     def __enter__(self):
         self.image = Image.open(self.file)
@@ -17,6 +22,15 @@ class Memer:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.image.close()
+        self.image = None
+        self.draw = None
+
+    def open(self):
+        self.image = Image.open(self.file)
+        self.draw = ImageDraw.Draw(self.image)
+
+    def close(self):
         self.image.close()
         self.image = None
         self.draw = None
@@ -47,6 +61,7 @@ class Memer:
         inspiration: https://tutorials.botsfloor.com/putting-text-on-images-using-python-part-2-cfc173c04874
         get text and it will return formatted text with newlines characters that can fit into image
         '''
+        self._check_if_its_opened()
         if max_width is None:
             max_width = self.image.width
         max_width -= spacing * 2
@@ -66,6 +81,7 @@ class Memer:
         return multiline_text
 
     def generate_meme(self, bottom_text: str, top_text: str):
+        self._check_if_its_opened()
         if top_text:
             self.write_centered(top_text, 10)
         if bottom_text:
@@ -73,6 +89,7 @@ class Memer:
             self.write_centered(bottom_text, y)
 
     def write_centered(self, text: str, y: int):
+        self._check_if_its_opened()
         text = self.text_wrap(text, self.image.width)
         x = self.calculate_text_center(text)
         self.draw.text((x, y), text, font=self.font, align="center")
@@ -82,6 +99,12 @@ class Memer:
         will return image in array of bytes
         :return:
         '''
+        self._check_if_its_opened()
         img_io = io.BytesIO()
         self.image.save(img_io, format="PNG")
         return img_io.getvalue()
+
+    def _check_if_its_opened(self):
+        if self.image is None or self.draw is None:
+            raise ValueError("u didnt opened file, please use 'with Memer() as m:'"
+                             " or 'Memer().open()', but then  dont forget to close it with close()")
